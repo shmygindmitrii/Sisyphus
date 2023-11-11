0dbe467b57a0df6372b774233942f80d1b6e778d
shmygindmitrii <shmygindmitrii@gmail.com>
2023-11-12 00:20:19 +0800

    add forgotten viewport attributes and methods to repo

--- Bonfire/inc/bonfire_raw_canvas.h
+++ Bonfire/inc/bonfire_raw_canvas.h
@@ -11,15 +11,18 @@ namespace Temple {
             int m_width = 0;
             int m_height = 0;
             int m_bytesPerPixel = 0;
+            Base::vec3 m_viewportMin, m_viewportMax;
         public:
             RawCanvas(int width, int height, int bytesPerPixel);
-            void resize(int width, int height, int bytesPerPixel);
             const uint8_t* getData() const;
+            void resize(int width, int height, int bytesPerPixel);
+            void setViewport(float xMin, float yMin, float zMin, float xMax, float yMax, float zMax);
             void putPixelStraight(int x, int y, const col4u& color);
             void putPixel(int x, int y, const col4u& color);
+            Base::vec4 processVertex(const Base::vec4& v);
             void fill(const col4u& color);
-            void drawLine(const Base::vec3 &a, const Base::vec3& b, const col4u& color);
-            void drawTriangle(const Base::vec3& a, const Base::vec3& b, const Base::vec3& c, const col4u& color);
+            void drawLine(const Base::vec4 &a, const Base::vec4& b, const col4u& color);
+            void drawTriangle(const Base::vec4& a, const Base::vec4& b, const Base::vec4& c, const col4u& color);
             ~RawCanvas();
         };
     }
