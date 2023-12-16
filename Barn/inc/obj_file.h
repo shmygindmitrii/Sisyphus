0523ebaf249a01d85f79879150cb98d4921a324f
shmygindmitrii <shmygindmitrii@gmail.com>
2023-12-16 14:40:21 +0800

    add Barn project
    contains only obj file handler

---
+++ Barn/inc/obj_file.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "base_vectors.h"
+
+#include <vector>
+#include <memory>
+
+namespace Temple {
+	namespace Barn {
+        struct ObjFaceIndex
+        {
+            unsigned int position;
+            unsigned int texture;
+            unsigned int normal;
+        };
+        struct ObjFace
+        {
+#if 0
+            unsigned int index_count;
+            ObjFaceIndex indices[4];
+#else
+            ObjFaceIndex indices[3];
+#endif
+        };
+
+        struct ObjFile {
+            std::vector<Base::vec3> coord;
+            std::vector<Base::vec2> uv;
+            std::vector<Base::vec3> normal;
+            std::vector<ObjFace>    faces;
+        };
+        std::shared_ptr<ObjFile> ReadObj(const char* path);
+	}
+}
---
+++ Barn/src/obj_file.cpp
@@ -0,0 +1,69 @@
+#include "obj_file.h"
+
+#include <fstream>
+#include <sstream>
+#include <memory>
+
+std::shared_ptr<Temple::Barn::ObjFile> Temple::Barn::ReadObj(const char* filename) {
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return nullptr;
+    }
+    std::shared_ptr<ObjFile> objPtr = std::make_shared<ObjFile>();
+    std::string line;
+    while (getline(file, line)) {
+        std::istringstream iss(line);
+        std::string prefix;
+        iss >> prefix;
+        if (prefix == "v") {
+            // vertex
+            Base::vec3 v;
+            iss >> v.x >> v.y >> v.z;
+            objPtr->coord.push_back(v);
+        }
+        else if (prefix == "vt") {
+            // texture 
+            Base::vec2 t;
+            iss >> t.u >> t.v;
+            objPtr->uv.push_back(t);
+        }
+        else if (prefix == "vn") {
+            // normal
+            Base::vec3 n;
+            iss >> n.x >> n.y >> n.z;
+            objPtr->normal.push_back(n);
+        }
+        else if (prefix == "f") {
+            ObjFace face;
+            std::string vertex;
+            int faceVertIdx = 0;
+            while (iss >> vertex) {
+                std::istringstream vertexStream(vertex);
+                std::string index;
+                int idx;
+
+                // Vertex index
+                getline(vertexStream, index, '/');
+                idx = std::stoi(index);
+                face.indices[faceVertIdx].position = idx;
+
+                // Texture index
+                if (getline(vertexStream, index, '/')) {
+                    idx = std::stoi(index);
+                    face.indices[faceVertIdx].texture = idx;
+                }
+
+                // Normal index
+                if (getline(vertexStream, index)) {
+                    idx = std::stoi(index);
+                    face.indices[faceVertIdx].normal = idx;
+                }
+                faceVertIdx++;
+            }
+            objPtr->faces.push_back(face);
+        }
+    }
+    return objPtr;
+}
